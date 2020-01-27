a6b958c35e9618bac6682b6413ce50943668eda6
Andrew Binnie <binnieandrew@gmail.com>
2020-01-27 16:31:14 +0000

    adding the waterbottle test and class setup files

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,14 +2,10 @@ public class Calculator {
 
     private int num1;
     private int num2;
-//    private double dnum1;
-//    private double dnum2;
-
 
     public Calculator(int num1, int num2) {
         this.num1 = num1;
         this.num2 = num2;
-
     }
 
 
